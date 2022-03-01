7704ed94beace0e7a81d631602a8c2f7c21f786b
weikang120 <leeweikang1220@gmail.com>
2022-03-01 21:44:20 +0800

    set create post

--- app/Views/post_list.php
+++ app/Views/post_list.php
@@ -11,7 +11,11 @@
     <link rel="stylesheet" href="<?= base_url("css/post_list.css") ?>">
 
     <div id="post-list">
-        <table id="post-table">
+        <div class="search-box">
+            <div style="font-size: 1.2em">Search: <input class="searchbar" type="text"></div>
+            <button class="btn btn-success" style="margin-left: auto; margin-right: 30px;" onclick="open_createModal()">New Post</button>
+        </div>
+        <table id="post-table" style="margin-top: 20px">
             <col width="8%">
             <col width="8%">
             <col width="32%">
@@ -31,12 +35,40 @@
             </tbody>
         </table>
         <div id="paging-container">
-            <button id="first-btn" class='btn btn-paging-ctrl'>first</button>
-            <button id="prev-btn" class='btn btn-paging-ctrl'>prev</button>
+            <button id="first-btn" class='btn btn-success'>first</button>
+            <button id="prev-btn" class='btn btn-success'>prev</button>
             <div id="page-btn-container"></div>
-            <button id="next-btn" class='btn btn-paging-ctrl'>next</button>
-            <button id="last-btn" class='btn btn-paging-ctrl'>last</button>
+            <button id="next-btn" class='btn btn-success'>next</button>
+            <button id="last-btn" class='btn btn-success'>last</button>
         </div>
+    </div>
+
+    <div id="create-modal">
+        <h2 style="text-align: center">New Post</h2>
+        <div style="margin-top: 15px; padding: 0 15px;">
+            <div style="font-size: 1.3em; display: flex; align-items:center;"><b>Author: </b><input id="create-author" type="number"></div>
+            <div style="font-size: 1.5em; margin-top: 15px;"><b>Title:</b></div>
+            <textarea id="create-title" class="title-textarea"></textarea>
+            <div style="margin-top: 30px; font-size: 1.5em;"><b>Body:</b></div>
+            <textarea id="create-body" class="body-textarea"></textarea>
+        </div>
+        <div style="text-align: center; margin-top: 30px">
+            <button class='btn btn-primary' onclick="open_createSubmitModal()">Create</button>
+            <button class='btn btn-secondary' style="margin-left: 25px;" onclick="close_createModal()">Cancel</button>
+        </div>
+    </div>
+
+    <div id="create-submit-modal">
+        <div style="font-size: 1.3em">Are you sure to create the post?</div>
+        <div style="margin-top: 20px">
+            <button class='btn btn-warning' onclick="submit_create()">Confirm</button>
+            <button class='btn btn-secondary' style="margin-left: 25px;" onclick="close_createSubmitModal()">Cancel</button>
+        </div>
+    </div>
+
+    <div id="create-result-modal" class="result-modal">
+        <div style="font-size: 1.2em;"><b></b></div>
+        <div><button style="margin-top: 20px;" class='btn btn-primary' onclick="close_all_modal()">Close</button></div>
     </div>
 
     <div id="view-modal">
@@ -56,11 +88,11 @@
     <div id="update-modal">
         <h2 id="update-post-id" style="text-align: center"></h2>
         <div id="update-author" style="text-align: center; font-size: 15px; margin-top: 5px;"></div>
-        <div style="margin-top: 30px; padding: 0 15px;">
+        <div style="margin-top: 15px; padding: 0 15px;">
             <div style="font-size: 1.5em;"><b>Title:</b></div>
-            <textarea id="update-title"></textarea>
+            <textarea id="update-title" class="title-textarea"></textarea>
             <div style="margin-top: 30px; font-size: 1.5em;"><b>Body:</b></div>
-            <textarea id="update-body"></textarea>
+            <textarea id="update-body" class="body-textarea"></textarea>
         </div>
         <div style="text-align: center; margin-top: 30px">
             <button class='btn btn-primary' onclick="open_updateSubmitModal()">Update</button>
@@ -76,7 +108,7 @@
         </div>
     </div>
 
-    <div id="update-result-modal">
+    <div id="update-result-modal" class="result-modal">
         <div style="font-size: 1.2em;"><b></b></div>
         <div><button style="margin-top: 20px;" class='btn btn-primary' onclick="close_all_modal()">Close</button></div>
     </div>
@@ -89,7 +121,7 @@
         </div>
     </div>
     
-    <div id="delete-result-modal">
+    <div id="delete-result-modal" class="result-modal">
         <div style="font-size: 1.2em;"><b></b></div>
         <div><button style="margin-top: 20px;" class='btn btn-primary' onclick="close_all_modal()">Close</button></div>
     </div>
@@ -164,6 +196,61 @@
             open_deleteModal();
         }
 
+        function open_createModal(){
+            $("#post-list").css("opacity", 0.1);   // fade the table
+            $(".btn-cell button").css("cursor", "default");   // remove hand cursor on button
+            $(".btn-cell button").prop("disabled", true);   // disable the buttons function
+            
+            $("#create-modal").css("display", "block");   // show post modal
+        }
+
+        function close_createModal(){
+            $("#create-modal").css("display", "none");   // hide create post modal
+            
+            $("#post-list").css("opacity", 1);   // set the table opacity higher
+            $(".btn-cell button").css("cursor", "hand");   // set hand cursor on button
+            $(".btn-cell button").prop("disabled", false);   // enable the buttons function
+        }
+
+        function open_createSubmitModal(){
+            $("#create-modal").css("opacity", 0.8);   // fade the update modal
+            $("#create-modal button").css("cursor", "default");   // remove hand cursor on button
+            $("#create-modal button").prop("disabled", true);   // disable the buttons function
+            
+            $("#create-submit-modal").css("display", "flex");
+        }
+
+        function close_createSubmitModal(){
+            $("#create-submit-modal").css("display", "none");   // hide post modal
+            
+            $("#create-modal").css("opacity", 1);   // set the update modal opacity higher
+            $("#create-modal button").css("cursor", "hand");   // set hand cursor on button
+            $("#create-modal button").prop("disabled", false);   // enable the buttons function
+        }
+
+        function open_createResultModal(result, message){
+            close_createSubmitModal();
+            
+            $("#create-result-modal").css("display", "block");
+
+
+            if(result == "success"){
+                $("#create-result-modal b").css("color", "green");
+            }else{
+                $("#create-result-modal b").css("color", "red");
+            }
+
+            $("#create-result-modal b").html(message);
+        }
+
+        function close_createResultModal(){
+            $("#create-submit-modal").css("opacity", 1);   // set the update confirm modal opacity higher
+            $("#create-submit button").css("cursor", "hand");   // set hand cursor on button
+            $("#create-submit button").prop("disabled", false);   // enable the buttons function
+            
+            $("#create-result-modal").css("display", "none");
+        }
+
         function open_viewModal(){
             $("#post-list").css("opacity", 0.1);   // fade the table
             $(".btn-cell button").css("cursor", "default");   // remove hand cursor on button
@@ -212,21 +299,19 @@
             $("#update-modal button").prop("disabled", false);   // enable the buttons function
         }
 
-        function open_updateResultModal(result){
-            $("#update-submit-modal").css("opacity", 0.8);   // fade the update confirm modal
-            $("#update-submit button").css("cursor", "default");   // remove hand cursor on button
-            $("#update-submit button").prop("disabled", true);   // disable the buttons function
+        function open_updateResultModal(result, message){
+            close_updateSubmitModal();
             
             $("#update-result-modal").css("display", "block");
 
 
             if(result == "success"){
                 $("#update-result-modal b").css("color", "green");
-                $("#update-result-modal b").html("Update Successfully");
             }else{
                 $("#update-result-modal b").css("color", "red");
-                $("#update-result-modal b").html("Update Failed. Try Again Later");
             }
+
+            $("#update-result-modal b").html(message);
         }
 
         function close_updateResultModal(){
@@ -254,9 +339,7 @@
         }
 
         function open_deleteResultModal(result){
-            $("#delete-modal").css("opacity", 0.8);   // fade the update confirm modal
-            $("#delete-modal button").css("cursor", "default");   // remove hand cursor on button
-            $("#delete-modal button").prop("disabled", true);   // disable the buttons function
+            close_deleteModal();
             
             $("#delete-result-modal").css("display", "block");
 
@@ -278,6 +361,10 @@
         }
 
         function close_all_modal(){
+            close_createResultModal();
+            close_createSubmitModal();
+            close_createModal();
+
             close_updateResultModal();
             close_updateSubmitModal();
             close_updateModal();
@@ -286,6 +373,20 @@
             close_deleteModal();
             
             AJAX_get_posts(currentPage);   // refresh table
+        }
+
+        function submit_create(){
+            var post_author = $("#create-author").val();
+            var post_title = $("#create-title").val();
+            var post_body = $("#create-body").val();
+            
+            var data = {
+                "user_id": post_author,
+                "title": post_title,
+                "body": post_body
+            };
+
+            AJAX_create_posts(data);
         }
 
         function submit_update(){
@@ -307,8 +408,6 @@
         function submit_delete(){
             AJAX_delete_posts(deletePostSelected);
         }
-
-        
 
         function UI_setPagingButton(){
             if(currentPage == 1){
@@ -376,9 +475,40 @@
             });
         }
 
+        function AJAX_create_posts(data){
+
+            console.log(data.user_id);
+            // AJAX call to update post
+            var xml = $.ajax({
+                url: "https://gorest.co.in/public/v2/posts",
+                method: "POST",
+                dataType: 'json',
+                data: data,
+                headers: {
+                    "Authorization": "Bearer b16cef22dd918befa7ed4cad3bb4b161a66a0d49bb582845a8cd7a398b0e8a70"
+                },
+                success: function(data, status){
+                    open_createResultModal("success", "Upload Post Successfully");
+                },
+                error: function(data){
+                    var body = xml.responseJSON;
+                    var message = "";
+
+                    if(body.length > 0){
+                        message += body[0].field + " ";
+                        message += body[0].message;
+                    }else{
+                        message += body.message;
+                    }
+
+                    open_createResultModal("error", message);
+                }
+            });
+        }
+
         function AJAX_update_posts(data){
             // AJAX call to update post
-            $.ajax({
+            var xml = $.ajax({
                 url: "https://gorest.co.in/public/v2/posts/" + data.id,
                 method: "PATCH",
                 dataType: 'json',
@@ -387,11 +517,20 @@
                     "Authorization": "Bearer b16cef22dd918befa7ed4cad3bb4b161a66a0d49bb582845a8cd7a398b0e8a70"
                 },
                 success: function(data, status){
-                    open_updateResultModal("success");
+                    open_updateResultModal("success", "Update Successfully");
                 },
                 error: function(data){
-                    open_updateResultModal("fail");
-                    console.log("Something error");
+                    var body = xml.responseJSON;
+                    var message = "";
+
+                    if(body.length > 0){
+                        message += body[0].field + " ";
+                        message += body[0].message;
+                    }else{
+                        message += body.message;
+                    }
+
+                    open_updateResultModal("error", message);
                 }
             });
         }
